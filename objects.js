ec4cade0d090bc1a1aa26b43fe3fdeec42add19c
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-17 03:04:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {artistName: 'Song Title'};
 
 function updatePlaylist(playlist,artistName,songTitle){
-  var newPlaylist = Object.assign({}, artistName,songTitle);
+  var newPlaylist = Object.assign({}, playlist,{artistName : [songTitle] });
   return newPlaylist;
 }
