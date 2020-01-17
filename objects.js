30d7e83535c9eadb97ba315e161f36f685a85489
brave-zombie-1932 <daniela.sahagun@hotmail.com>
2020-01-17 03:23:21 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist,artistName,songTitle){
 }
 
 function removeFromPlaylist(playlist,artistName){
-  var updatedPlaylist = delete playlist.artistName;
-  return updatedPlaylist;
+  delete playlist.artistName;
+  return playlist;
 }
